617ab90fccbb9492f7c4692ed086de97893fba2a
Vladan Colovic <cvladan@gmail.com>
2025-02-18 14:30:21 +0100

    Refactor plugin header comments

--- plugin.php
+++ plugin.php
@@ -1,16 +1,19 @@
 <?php
 
-/*
-Plugin Name: Lexicon | SVKO
-Plugin URI: https://dennishoppe.de/en/wordpress-plugins/encyclopedia
-Description: Patched slugs inside "post-type-labels.php" and "cross-linker.php" flags. Encyclopedia enables you to create your own encyclopedia, lexicon, glossary, wiki, dictionary or knowledge base.
-Version: 1.7.58
-Author: Dennis Hoppe
-Author URI: https://DennisHoppe.de
-Text Domain: lexicon
-Domain Path: /languages
-Update URI: false
-*/
+/**
+ * Plugin Name:     Lexicon | SVKO
+ * Plugin URI:      https://dennishoppe.de/en/wordpress-plugins/encyclopedia
+ * Description:     Patched slugs inside "post-type-labels.php" and "cross-linker.php" flags. Encyclopedia enables you to create your own encyclopedia, lexicon, glossary, wiki, dictionary or knowledge base.
+ * Version:         1.7.58
+ * Author:          Dennis Hoppe
+ * Author URI:      https://svko.com
+ * License:         GPL-2.0-or-later
+ * License URI:     http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:     lexicon
+ * Domain Path:     /languages
+ * Update URI:      false
+ * Requires PHP:      8.0
+ */
 
 if (version_compare(PHP_VERSION, '7.4', '<')) {
     die(sprintf('Your PHP version (%s) is far too old. <a href="https://secure.php.net/supported-versions.php" target="_blank">Please upgrade immediately.</a> Then activate the plugin again.', PHP_VERSION));
