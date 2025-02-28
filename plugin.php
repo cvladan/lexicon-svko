ff674e2fb719e0c1f56dab5462e62a32534a6b34
Vladan Colovic <cvladan@gmail.com>
2025-02-28 11:26:53 +0100

    Change text domain loading action to 'init'

--- plugin.php
+++ plugin.php
@@ -22,15 +22,15 @@ PHP_VERSION_ID < 80000 && die("PHP 8.0+ required. Your version: {$PHP_VERSION}")
 
 if (!defined($_ = __NAMESPACE__)) : define($_, 1); # prevent redeclaration; singleton; don't forget endif
 
-  // Load text domain
-  add_action('plugins_loaded', function() {
-    load_plugin_textdomain('lexicon-svko', false, basename(dirname(__FILE__)) . '/languages');
-  });
+// Load text domain
+add_action('init', function() {
+  load_plugin_textdomain('lexicon-svko', false, basename(dirname(__FILE__)) . '/languages');
+});
 
-  foreach (glob(realpath(__DIR__ . '/includes') . '/*.php') as $inc) {
-    require_once $inc;
-  }
+foreach (glob(realpath(__DIR__ . '/includes') . '/*.php') as $inc) {
+  require_once $inc;
+}
 
-  Core::init(__FILE__);
+Core::init(__FILE__);
 
 endif;
